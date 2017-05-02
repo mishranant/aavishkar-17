66d5d9a57355ef590002772f995f4a1dbcbadb9a
AMRENDRA SINGH <ashking13th@users.noreply.github.com>
2017-05-02 16:40:11 +0530

    Add files via upload

--- arduino1/config.h
+++ arduino1/config.h
@@ -1,2 +1,24 @@
-//pin and variable configurations for Arduino 1
-// INPUTS
+/**
+ * Pin definitions
+ */
+
+ //MOTORS motor 1 : front ; 2 : back
+  int leftMotor11 = 0;
+  int leftMotor11 = 0;
+  
+  int leftMotor21 = 0;
+  int leftMotor22 = 0;
+  
+  int rightMotor11 = 0;
+  int rightMotor12 = 0;
+  
+  int rightMotor21 = 0;
+  int rightMotor22 = 0;
+
+ //MOTOR calibration
+  int leftMotor1Value = 0;
+  int leftMotor2Value = 0;
+  int rightMotor1Value = 0;
+  int rightMotor2Value = 0;
+
+  
---
+++ arduino1/move.ino
@@ -0,0 +1,108 @@
+/**
+ * Functions to make the car move 
+ */
+
+//**DONE
+   void moveFront()
+   {
+    analogWrite( leftMotor11 , leftMotor1Value );
+    analogWrite( leftMotor12 , 0 );
+    
+    analogWrite( leftMotor21 , leftMotor2Value );
+    analogWrite( leftMotor22 , 0 );
+    
+    analogWrite( rightMotor11 , rightMotor1Value );
+    analogWrite( rightMotor12 , 0 );
+    
+    analogWrite( rightMotor21 , rightMotor2Value );
+    analogWrite( rightMotor22 , 0 );
+    
+   }
+  
+   void rightFront()
+   {
+    analogWrite( leftMotor11 , leftMotor1Value );
+    analogWrite( leftMotor12 , 0 );
+    
+    analogWrite( leftMotor21 , leftMotor2Value );
+    analogWrite( leftMotor22 , 0 );
+    
+    analogWrite( rightMotor11 , 0 );
+    analogWrite( rightMotor12 , rightMotor1Value );
+    
+    analogWrite( rightMotor21 , 0 );
+    analogWrite( rightMotor22 , rightMotor2Value );
+    
+   }
+  
+   void leftFront()
+   {
+    analogWrite( leftMotor11 , 0 );
+    analogWrite( leftMotor12 , leftMotor1Value );
+    
+    analogWrite( leftMotor21 , 0 );
+    analogWrite( leftMotor22 , leftMotor2Value );
+    
+    analogWrite( rightMotor11 , rightMotor1Value );
+    analogWrite( rightMotor12 , 0 );
+    
+    analogWrite( rightMotor21 , rightMotor2Value );
+    analogWrite( rightMotor22 , 0 );
+    
+   }
+  
+   void moveBack()
+   {
+    analogWrite( leftMotor11 , 0 );
+    analogWrite( leftMotor12 , leftMotor1Value );
+    
+    analogWrite( leftMotor21 , 0 );
+    analogWrite( leftMotor22 , leftMotor2Value );
+    
+    analogWrite( rightMotor11 , 0 );
+    analogWrite( rightMotor12 , rightMotor1Value );
+    
+    analogWrite( rightMotor21 , 0 );
+    analogWrite( rightMotor22 , rightMotor2Value );
+    
+   }
+  
+   void rightBack()
+   {
+    analogWrite( leftMotor11 , 0 );
+    analogWrite( leftMotor12 , leftMotor1Value );
+    
+    analogWrite( leftMotor21 , 0 );
+    analogWrite( leftMotor22 , leftMotor2Value );
+    
+    analogWrite( rightMotor11 , rightMotor1Value );
+    analogWrite( rightMotor12 , 0 );
+    
+    analogWrite( rightMotor21 , rightMotor2Value );
+    analogWrite( rightMotor22 , 0 );
+    
+   }
+  
+   void leftBack()
+   {
+    analogWrite( leftMotor11 , leftMotor1Value );
+    analogWrite( leftMotor12 , 0 );
+    
+    analogWrite( leftMotor21 , leftMotor2Value );
+    analogWrite( leftMotor22 , 0 );
+    
+    analogWrite( rightMotor11 , 0 );
+    analogWrite( rightMotor12 , rightMotor1Value );
+    
+    analogWrite( rightMotor21 , 0 );
+    analogWrite( rightMotor22 , rightMotor2Value );
+    
+   }
+
+   void breaker()
+   {
+    
+   }
+
+ 
+
