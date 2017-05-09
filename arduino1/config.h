c3903f8bdef26ee3fb176afdfa83e410f3b07ab1
Nikhil T R <nakuscorpion98@gmail.com>
2017-05-09 22:51:45 +0530

    Pins for sonars
    Added pin definitions for the 8 sonars and Data structure for controlling sonar,

--- arduino1/config.h
+++ arduino1/config.h
@@ -21,4 +21,43 @@
   int rightMotor1Value = 0;
   int rightMotor2Value = 0;
 
+typedef struct sonar{
+  const int trigpin, echopin;
+  float distmax;
+}sonar;
+sonar sonars[8];
+
+sonars[0].trigpin = 9;
+sonars[0].echopin = 10;
+sonars[0].distmax = 30.0;
+
+sonars[1].trigpin = 9;
+sonars[1].echopin = 10;
+sonars[1].distmax = 30.0;
+
+sonars[2].trigpin = 9;
+sonars[2].echopin = 10;
+sonars[2].distmax = 30.0;
+
+sonars[3].trigpin = 9;
+sonars[3].echopin = 10;
+sonars[3].distmax = 30.0;
+
+sonars[4].trigpin = 9;
+sonars[4].echopin = 10;
+sonars[4].distmax = 30.0;
+
+sonars[5].trigpin = 9;
+sonars[5].echopin = 10;
+sonars[5].distmax = 30.0;
+
+sonars[6].trigpin = 9;
+sonars[6].echopin = 10;
+sonars[6].distmax = 30.0;
+
+sonars[7].trigpin = 9;
+sonars[7].echopin = 10;
+sonars[7].distmax = 30.0;
+
+
   
