c59039fdbd88a7b8511b0b0823129b3a45b9db05
Nikhil T R <nakuscorpion98@gmail.com>
2017-05-09 23:04:49 +0530

    Sonar functions
    This file contains code for setupSonar() and readSonar()

---
+++ arduino1/setupSonar.ino
@@ -0,0 +1,31 @@
+
+#ifndef SONAR
+#define SONAR
+typedef struct sonar{
+  const int trigpin, echopin;
+  float distmax;
+}sonar;
+#endif
+
+void setupSonars() {
+  for(int i=0; i<8; i++){
+    pinMode(sonars[i].trigpin, OUTPUT);
+    pinMode(sonars[i].echopin, INPUT);
+    }
+}
+
+float readSonar(sonar sonar_id)
+{
+  digitalWrite(sonar_id.trigpin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(sonar_id.trigpin, HIGH);//triggering the sonar to emit an ultrasonic wave
+  delayMicroseconds(5);
+  digitalWrite(sonar_id.trigpin, LOW);//it is actually triggered at this instant
+  
+  duration = pulseIn(sonar_id.echoPin, HIGH ,((sonar_id.distmax+13)*2.0)/.034 );//time taken by reflected wave to be detected
+  if (duration == 0) return -1.0;//object was out of range
+  else {
+    return (duration*0.034)/2.0;
+  }
+}
+
