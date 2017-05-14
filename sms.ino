bb753c7bb1df7bd5bf91112f1a62f51b2c162714
Chirag Vashist <chiragvashist007@gmail.com>
2017-05-14 19:00:47 +0530

    GSM code
    Tx -> 10
    Rx -> 11

---
+++ sms.ino
@@ -0,0 +1,26 @@
+#include<SoftwareSerial.h>
+
+SoftwareSerial sms(7,8);
+int i;
+void setup() {
+  // put your setup code here, to run once:
+  sms.begin(9600);
+  delay(100);
+  Serial.begin(9600);
+i = 1;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (i == 1) {
+  sms.write("AT+CMGF=1\r\n");
+  delay(100);
+  sms.write("AT+CMGS=\"+918894041783\"\r\n");
+  delay(100);
+  sms.write("Stay Alert! Best wishes, ACS Lab, IIT Mandi ");
+  delay(100);
+  sms.write((char)26);
+  delay(100);
+  i++;
+  }
+}
