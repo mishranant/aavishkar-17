599301046cec374cd3a30b2c4851e122d0c7bc74
aditya1297 <adityasingh1297@gmail.com>
2017-05-14 18:47:00 +0530

    Update buzzer.h

--- arduino1/buzzer.h
+++ arduino1/buzzer.h
@@ -7,41 +7,7 @@ void setup() {
 
 void loop() {
     // put your main code here, to run repeatedly:
-  /*
-  int a;
-  int i;
-  a=2;
-
- if(a==1)
- {
-    for(i=0;i<5;i++)
-    {
-        tone(buzzer, 1000); 
-        delay(100);        
-         noTone(buzzer);
-         delay(100);
-      }
-  }
-  else if(a==2)
-  {
-    for(i=0;i<5;i++){
-    tone(buzzer, 1500); 
-        delay(250);        
-         noTone(buzzer);
-         delay(250);
-    }
-    }
-    else if(a==3)
-    {
-       for(i=0;i<5;i++)
-       {
-    tone(buzzer, 400); 
-        delay(500);        
-         noTone(buzzer);
-         delay(500);
-    }
-    }
-    */
+ 
     int a;
     a=3;
     beep(a);
