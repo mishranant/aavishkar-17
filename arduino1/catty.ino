2be63364511ef49083dee7e920378fd211bdc065
Akai9866 <b16036@students.iitmandi.ac.in>
2017-05-18 17:48:56 +0530

    Automatic motor configuration

---
+++ arduino1/catty.ino
@@ -0,0 +1,32 @@
+int m1a = 255, m2a = 255, m3a = 255, m4a = 255;
+
+void motorWrite()
+{
+  analogWrite(m1, m1a);
+  analogWrite(m2, m2a);
+  analogWrite(m3, m3a);
+  analogWrite(m4, m4a);
+}
+
+void setup() {
+  Serial.begin(9600);
+  motorWrite();
+}
+
+void loop() {
+  //readAccelerometer
+  if(accx > 1)
+  {
+    m2a--;
+    m3a--;
+    motorWrite();
+    delay(100);
+  }
+  else if(accx < -1)
+  {
+    m1a--;
+    m4a--;
+    motorWrite();
+    delay(100);
+  }
+}
